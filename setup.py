6b759bad8a2c99d62cbdfa370b98eb1daa1ad875
Wojtek Siudzinski <admin@suda.pl>
2016-03-13 17:53:23 +0000

    Fix setup script

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['artwork', 'docs', 'tests']),
+    packages=find_packages(exclude=['artwork', 'docs', 'examples', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
